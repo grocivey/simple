a1aa4fa1597cfcbbf28628518aad9a145462a627
gro <-->
2023-08-05 13:55:56 +0800

    打包配置提交

--- springapi/src/main/java/com/simple/api/config/RedisConfig.java
+++ springapi/src/main/java/com/simple/api/config/RedisConfig.java
@@ -54,32 +54,32 @@ public class  RedisConfig<K,V> {
 
     }
 
-    @Bean("myStringRedisTemplate")
-    public RedisTemplate<String, Object> stringRedisTemplate(RedisConnectionFactory factory) {
-
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-
-        template.setConnectionFactory(factory);
-
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-
-        // key采用String的序列化方式
-        template.setKeySerializer(stringRedisSerializer);
-
-        // hash的key也采用String的序列化方式
-        template.setHashKeySerializer(stringRedisSerializer);
-
-        // value序列化方式采用jackson
-        template.setValueSerializer(stringRedisSerializer);
-
-        // hash的value序列化方式采用jackson
-        template.setHashValueSerializer(stringRedisSerializer);
-
-        template.afterPropertiesSet();
-
-        return template;
-
-    }
+//    @Bean("myStringRedisTemplate")
+//    public RedisTemplate<String, Object> stringRedisTemplate(RedisConnectionFactory factory) {
+//
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//
+//        template.setConnectionFactory(factory);
+//
+//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+//
+//        // key采用String的序列化方式
+//        template.setKeySerializer(stringRedisSerializer);
+//
+//        // hash的key也采用String的序列化方式
+//        template.setHashKeySerializer(stringRedisSerializer);
+//
+//        // value序列化方式采用jackson
+//        template.setValueSerializer(stringRedisSerializer);
+//
+//        // hash的value序列化方式采用jackson
+//        template.setHashValueSerializer(stringRedisSerializer);
+//
+//        template.afterPropertiesSet();
+//
+//        return template;
+//
+//    }
 
 
 }
