9599268b4627e23195abdbf30715b31f2ce78dfb
gro <-->
2023-08-22 17:01:58 +0800

    1

--- simple-common/src/main/java/com/gzy/GzyConfig.java
+++ simple-common/src/main/java/com/gzy/GzyConfig.java
@@ -2,6 +2,9 @@ package com.gzy;
 
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * spring.factories配置该类自动配置
+ */
 @ComponentScan(basePackages = "com.gzy")
 public class GzyConfig {
 }
