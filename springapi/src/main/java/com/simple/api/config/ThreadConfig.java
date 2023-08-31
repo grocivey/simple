84b89b558a99150d867e7828dd8b09a3c515b149
gro <-->
2023-08-31 15:39:06 +0800

    全局捕获线程池异常

--- springapi/src/main/java/com/simple/api/config/ThreadConfig.java
+++ springapi/src/main/java/com/simple/api/config/ThreadConfig.java
@@ -1,12 +1,12 @@
 package com.simple.api.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 
 @Slf4j
+@Configuration
 @Order(1)
-@Component
 public class ThreadConfig {
     static {
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> log.error("ThreadPool  {} got exception", t, e));
