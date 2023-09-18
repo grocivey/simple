c6f2b864c9929233426f51ff1d38ae9a626ded8f
gro <-->
2023-09-18 10:52:11 +0800

    gson serializeNulls

--- springapi/src/main/java/com/simple/api/util/GsonUtil.java
+++ springapi/src/main/java/com/simple/api/util/GsonUtil.java
@@ -1,6 +1,7 @@
 package com.simple.api.util;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.lang.reflect.Type;
 
@@ -9,7 +10,7 @@ import java.lang.reflect.Type;
  * @author gzy
  */
 public class GsonUtil {
-    public static final Gson GSON = new Gson();
+    public static final Gson GSON = new GsonBuilder().serializeNulls().create();
     private GsonUtil(){}
 
     public static <T> String toJson(T str){
